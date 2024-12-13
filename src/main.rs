1757af38b03ec66e84f6fd950c5ff0d758ba7244
KWilson718 <wilsonk18@spu.edu>
2024-12-13 10:31:21 -0800

    throwing more tests with success so far!

--- src/main.rs
+++ src/main.rs
@@ -48,4 +48,31 @@ fn test_scores() {
     let test_score = calculate_score(&test_hand);
 
     println!("Resulting Score is: {}\n", test_score);
+
+    let mut test_hand_2: Vec<Card> = Vec::new();
+
+    test_hand_2.push(create_card(Suit::Star, Value::Five, 5));
+    test_hand_2.push(create_card(Suit::Diamond, Value::Five, 5));
+    test_hand_2.push(create_card(Suit::Heart, Value::Five, 5));
+    test_hand_2.push(create_card(Suit::Club, Value::Five, 5));
+
+    let test_score_2 = calculate_score(&test_hand_2);
+
+    println!("Resulting Score is: {}", test_score_2);
+
+    let mut test_hand_3: Vec<Card> = Vec::new();
+
+    test_hand_3.push(create_card(Suit::Star, Value::Five, 5));
+    test_hand_3.push(create_card(Suit::Diamond, Value::Five, 5));
+    test_hand_3.push(create_card(Suit::Heart, Value::Five, 5));
+    test_hand_3.push(create_card(Suit::Club, Value::Five, 5));
+    
+    test_hand_3.push(create_card(Suit::Spade, Value::Five, 5));
+    test_hand_3.push(create_card(Suit::Spade, Value::Six, 6));
+    test_hand_3.push(create_card(Suit::Spade, Value::Seven, 7));
+    test_hand_3.push(create_card(Suit::Spade, Value::Eight, 8));
+
+    let test_score_3 = calculate_score(&test_hand_3);
+
+    println!("Resulting Score is: {}", test_score_3);
 }
