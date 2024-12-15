f07f3764069a388c51e3f214c962dc51a7e9af0f
KWilson718 <wilsonk18@spu.edu>
2024-12-15 12:02:52 -0800

    added ch 6 commentary to main

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,20 @@ use game_aids::game::{game, short_game};
 // Each option calls out a different function to handle different functionality of the game
 fn main() {
 
+    // Ch 6, Section 2, Primitive Types - Boolean Values
+    // The usage of a bool here is to clearly provide a switch between on and off for the while loop
+    let mut still_playing: bool = true; // Boolean used to dictate to the while loop that the game is still going
+
     // The While loop in this instance is used to simulate a potentially multi-game long session of usage, or the ability to read the rules & return to play
     // So long as the exit option isn't hit, then the game will continue looping through other logic and returning to the menu once ready to repeat
-    let mut still_playing: bool = true; // Boolean used to dictate to the while loop that the game is still going
     while still_playing {
         println!("Welcome to Command Line Five Crowns"); // Welcome message when menu hit
 
+        // Ch 6, Section 3, Strings
         // This string is used to send to the prompt for number function in order to show the options that the player can select from
         let menu_string = "1 - Play Five Crowns\n2 - Play Simplified Game\n3 - Play Single Round\n4 - Test Scores (Dev Tool)\n0 - Exit Game";
 
+        // Ch 6, Section 2, Primitive Data Types - Unsigned Int (there doesn't need to be a negative number)
         // Fetches the selection from the user through this generic function which prompts the user for a number
         let selection_val = prompt_for_number(menu_string, 0, 4);
 
